9ff37b13800377d46af883375e5a99527bbe7cd2
mostafagamalbisher <mostafagamalbisher@outlook.com>
2026-05-24 16:58:53 +0300

    add SideMenu component and utility function for class name merging

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from './components/Header';
+import SideMenu from './components/SideMenu';
 
 function App() {
   return (
-    <div>
+    <div className="flex h-screen flex-col">
       <Header />
+      <div className="flex flex-1">
+        <SideMenu />
+      </div>
     </div>
   );
 }
